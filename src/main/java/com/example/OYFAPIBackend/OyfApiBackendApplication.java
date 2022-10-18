60ddb3efdd5def1edc4b4180a230f816dcb30822
Evans Mbithi <evholmbithy@gmail.com>
2022-10-18 10:28:01 +0300

    Spring Initializer

---
+++ src/main/java/com/example/OYFAPIBackend/OyfApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.OYFAPIBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyfApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OyfApiBackendApplication.class, args);
+	}
+
+}
